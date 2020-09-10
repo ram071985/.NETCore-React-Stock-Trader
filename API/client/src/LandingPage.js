d5ec306baedd6347c103088f327b93d15a119d6d
ram071985 <reidmuchow@gmail.com>
2020-09-10 18:39:56 -0500

    added LandingPage component to src directory.

---
+++ API/client/src/LandingPage.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from "react";
+
+class LandingPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            
+        };
+    }
+
+    render(){
+        return(
+            <div>
+            <div className="container-fluid">
+                <div className="row h-100 ">
+                    <div className="col-lg h-100 left-col">
+                        <h3>Yo</h3>
+
+                    </div>
+                    <div className="col-lg h-100 right-col">
+                        <h3>yo                           
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            </div>
+        );
+    }
+
+
+  
+}
+
+export default LandingPage;
--- API/client/src/index.js
+++ API/client/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LandingPage from './LandingPage';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
