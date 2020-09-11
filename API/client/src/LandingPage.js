c9be5ae56755c8392090c93777039e094c4efe87
ram071985 <reidmuchow@gmail.com>
2020-09-10 19:56:57 -0500

    first week-one commit.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -13,11 +13,11 @@ class LandingPage extends Component {
             <div>
             <div className="container-fluid min-vh-100 d-flex flex-column">
                 <div className="row flex-grow-1">
-                    <div className="col-lg left-col">
+                    <div className="col-lg d-block mx-auto left-col">
                         <h3>Yo</h3>
 
                     </div>
-                    <div className="col-lg right-col">
+                    <div className="col-lg d-block mx-auto right-col">
                         <h3>yo                           
                         </h3>
                     </div>
