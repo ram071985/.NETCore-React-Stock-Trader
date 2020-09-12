ee3c45bc4d0cae9186d6d5f421ee1ce367a7bc65
ram071985 <reidmuchow@gmail.com>
2020-09-12 18:26:22 -0500

    added description text and colors.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -15,7 +15,9 @@ class LandingPage extends Component {
             <div className="container-fluid min-vh-100 d-flex flex-column">
                 <div className="row flex-grow-1 landing-row">
                     <div className="col-lg d-block mx-auto left-col">
-                        <h3>Yo</h3>
+                        <h1 className="text-left welcome-text">Welcome to Trade<span className="topia-color">Topia</span></h1>
+                        <h3 className="text-left second-description-text">A make believe stock trading resource.</h3>
+                        <h5 className="text-left description-text">Think of it as a way to practice trading and selling from public companies, but with no strings attached!</h5>
                     </div>
                     <div className="col-lg d-block mx-auto right-col">
                       <img className="splash-image d-block mx-auto" src={SplashImage}/>
