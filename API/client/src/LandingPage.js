c5b42d841f0bf82d3bb0e14a21e6787c26282743
ram071985 <reidmuchow@gmail.com>
2020-09-12 13:12:00 -0500

    added image and gradient to container background.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from "react";
+import SplashImage from "./images/splashImage.jpeg";
 
 class LandingPage extends Component {
     constructor() {
@@ -12,14 +13,13 @@ class LandingPage extends Component {
         return(
             <div>
             <div className="container-fluid min-vh-100 d-flex flex-column">
-                <div className="row flex-grow-1">
+                <div className="row flex-grow-1 landing-row">
                     <div className="col-lg d-block mx-auto left-col">
                         <h3>Yo</h3>
-
                     </div>
                     <div className="col-lg d-block mx-auto right-col">
-                        <h3>yo                           
-                        </h3>
+                      <img className="splash-image d-block mx-auto" src={SplashImage}/>
+                    
                     </div>
                 </div>
             </div>
