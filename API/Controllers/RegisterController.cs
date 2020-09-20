b67f4de6fa867c456fa8a2bfb25491dcef0fd654
ram071985 <reidmuchow@gmail.com>
2020-09-20 18:57:12 -0500

    added asynchronous handling to input boxes, statrted adding controllers and models.

---
+++ API/Controllers/RegisterController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Core.Services;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/register")]
+
+    public class RegisterContoller : ContollerBase
+    {
+        [HttpPost]
+        public
+    }
+}
