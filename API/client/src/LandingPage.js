3ee13c3ad34d1a811509f2853be0cf27c7d37e92
ram071985 <reidmuchow@gmail.com>
2020-09-20 19:38:17 -0500

    added /images to gitignore and changed overlay in landingpage background.

--- API/client/src/LandingPage.js
+++ API/client/src/LandingPage.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from "react";
+import SplashImage from "./images/splashImage.jpeg";
 
 class LandingPage extends Component {
     constructor() {
@@ -12,14 +13,15 @@ class LandingPage extends Component {
         return(
             <div>
             <div className="container-fluid min-vh-100 d-flex flex-column">
-                <div className="row flex-grow-1">
-                    <div className="col-lg left-col">
-                        <h3>Yo</h3>
-
+                <div className="row flex-grow-1 landing-row">
+                    <div className="col-lg d-block mx-auto left-col">
+                        <h1 className="text-left welcome-text">Welcome to Trade<span className="topia-color">Topia</span></h1>
+                        <h3 className="text-left second-description-text">A make believe stock trading resource.</h3>
+                        <h4 className="text-left description-text">A fun and simple way to practice trading and selling from public companies: with no strings attached.</h4>
                     </div>
-                    <div className="col-lg right-col">
-                        <h3>yo                           
-                        </h3>
+                    <div className="col-lg d-block mx-auto right-col">
+                      <img className="splash-image d-block mx-auto" src={SplashImage}/>
+                    
                     </div>
                 </div>
             </div>
