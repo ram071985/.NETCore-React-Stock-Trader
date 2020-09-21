2da8ad75c4655049f2d171dfeb3050e6b2b35525
ram071985 <reidmuchow@gmail.com>
2020-09-20 19:27:24 -0500

    recommited lost commits.

--- API/client/src/index.js
+++ API/client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LandingPage from './LandingPage';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
