f36e4e39114f8d8e7be0ae3a215559f437fea723
ram071985 <reidmuchow@gmail.com>
2020-11-21 08:27:43 -0600

    finished SellStockService tests.

--- CORE/Services/SellStockService.cs
+++ CORE/Services/SellStockService.cs
@@ -15,10 +15,14 @@ namespace CORE.Services
     public class SellStockService : ISellStockService
     {
         private readonly IDbSessionService _dbSessionService;
+        private readonly IWalletQueryService _walletQueryService;
+        private readonly IStockQueryService _stockQueryService;
 
-        public SellStockService(IDbSessionService dbSessionService)
+        public SellStockService(IDbSessionService dbSessionService, IWalletQueryService walletQueryService, IStockQueryService stockQueryService)
         {
             _dbSessionService = dbSessionService;
+            _walletQueryService = walletQueryService;
+            _stockQueryService = stockQueryService;
         }
 
         public Wallet UpdateWalletSale(int userId, decimal balance)
@@ -27,9 +31,7 @@ namespace CORE.Services
             {
                 using (var transaction = session.BeginTransaction())
                 {
-                    var result = session.QueryOver<Wallet>()
-                          .Where(w => w.UserId == userId)
-                          .List<Wallet>();
+                    var result = _walletQueryService.QueryWallets(session, userId);
 
                     result[0].Balance = result[0].Balance + balance;
                     result[0].Holdings = result[0].Holdings - balance;
@@ -73,10 +75,7 @@ namespace CORE.Services
             {
                 using (var transaction = session.BeginTransaction())
                 {
-                    var result = session.QueryOver<Stock>()
-                        .Where(s => s.UserId == userId)
-                        .Where(s => s.Symbol == symbol)
-                        .List<Stock>();
+                    var result = _stockQueryService.QueryStocks(session, userId, symbol);
 
                     if (result[0].Quantity - quantity > 0)
                     {
---
+++ Tests/SellStockServiceTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CORE.Entities;
+using CORE.Services;
+using NHibernate;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class SellStockServiceTest
+    {
+        private IDbSessionService _dbSessionService;
+        private IWalletQueryService _walletQueryService;
+        private IStockQueryService _stockQueryService;
+
+        private SellStockService _sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dbSessionService = Substitute.For<IDbSessionService>();
+            _walletQueryService = Substitute.For<IWalletQueryService>();
+            _stockQueryService = Substitute.For<IStockQueryService>();
+
+            _sut = new SellStockService(_dbSessionService, _walletQueryService, _stockQueryService);
+        }
+
+        [Test]
+        public void should_update_wallet_deposit_return()
+        {
+            Random rnd = new Random();
+
+            var userId = rnd.Next();
+
+            var balance = 100.90m;
+
+            var session = Substitute.For<ISession>();
+            _dbSessionService.OpenSession().Returns(session);
+
+            _walletQueryService.QueryWallets(Arg.Any<ISession>(), Arg.Any<int>())
+                .Returns(new List<Wallet>
+                {
+                    new Wallet
+                    {
+                        UserId = userId,
+                        Balance = 10356.30m,
+                        Holdings = 300.65m
+                    }
+                });
+
+            _sut.UpdateWalletSale(userId, balance);
+        }
+
+        [Test]
+        public void should_add_deposit_record_return()
+        {
+            Random rnd = new Random();
+
+            var userId = rnd.Next();
+
+            var exchange = CredentialRandomUtil.GetRandomString();
+
+            var deposit = 1000.23m;
+
+            var quantity = 50;
+
+            var session = Substitute.For<ISession>();
+            _dbSessionService.OpenSession().Returns(session);
+
+            _sut.AddDeposit(userId, exchange, deposit, quantity);
+        }
+
+        [Test]
+        public void should_create_sale_record_return()
+        {
+            Random rnd = new Random();
+
+            var userId = rnd.Next();
+
+            var company = CredentialRandomUtil.GetRandomString();
+
+            var symbol = CredentialRandomUtil.GetRandomString();
+
+            var quantity = 70;
+
+            var session = Substitute.For<ISession>();
+            _dbSessionService.OpenSession().Returns(session);
+
+            _sut.CreateSaleRecord(userId, company, symbol, quantity);
+        }
+
+        [Test]
+        public void should_update_sale_record_return()
+        {
+            Random rnd = new Random();
+
+            var userId = rnd.Next();
+
+            var company = CredentialRandomUtil.GetRandomString();
+
+            var symbol = CredentialRandomUtil.GetRandomString();
+
+            var quantity = 70;
+
+            var session = Substitute.For<ISession>();
+            _dbSessionService.OpenSession().Returns(session);
+
+            _stockQueryService.QueryStocks(Arg.Any<ISession>(), Arg.Any<int>(), Arg.Any<string>())
+                .Returns(new List <Stock>
+                {
+                    new Stock
+                    {
+                        UserId = userId,
+                        Symbol = symbol,
+                        Quantity = 28
+                    }
+                });
+
+            _sut.CreateSaleRecord(userId, company, symbol, quantity);
+        }
+
+        static class CredentialRandomUtil
+        {
+
+            public static string GetRandomString()
+            {
+                string path = Path.GetRandomFileName();
+                path = path.Replace(".", "");
+                return path;
+            }
+        }
+    }
+}
