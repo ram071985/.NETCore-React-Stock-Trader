6854a91ac524748e6c6b3d70ca8a3d950acd4315
ram071985 <reidmuchow@gmail.com>
2020-11-22 12:32:52 -0600

    changed padding in user portal containers.

--- API/client/src/Components/UserInfo.js
+++ API/client/src/Components/UserInfo.js
@@ -10,81 +10,79 @@ class UserInfo extends Component {
 
   render() {
     return (
-        <div className="container-fluid d-block container user-container">
-          <div className="d-block row">
-            <h4 className="d-inline-block ml-3 mr-3 heading-text">
-              User Information
-            </h4>
-            <Button
-              onClick={this.props.handleShow}
-              className="buy-sell-button"
-              variant="success"
-            >
-              <span className="font-weight-light">Buy/Sell Stocks</span>
-            </Button>{" "}
-            <hr style={{ borderTop: "1px solid #1aac3c", width: "100%" }} />
-            <div className="col-12">
-              <h6 className="font-weight-normal d-inline-block mb-1 titles-text">
-                User{" "}
-              </h6>
-              <h6 className="font-weight-light d-block mb-2 name-text">
-                {this.props.loading ? (
-                  <Spinner
-                    className="ml-2"
-                    variant="success"
-                    animation="border"
-                    size="sm"
-                  />
-                ) : (
-                  this.props.username
-                )}
-              </h6>
-              <h6
-                id="holdings"
-                className="font-weight-normal d-inline-block mb-1 titles-text"
-              >
-                Holdings:
-              </h6>
-              <h6
-                id="holding-text"
-                className="font-weight-light d-block mb-2 name-text"
-              >
-                $
-                {this.props.loading ? (
-                  <Spinner
-                    className="ml-2"
-                    variant="success"
-                    animation="border"
-                    size="sm"
-                  />
-                ) : (
-                  this.props.formatter().format(this.props.holdings)
-                )}
-              </h6>
-            </div>
-            <div className="col-5">
-              <h6
-                id="wallet"
-                className="font-weight-normal d-inline-block mb-1 titles-text"
-              >
-                Wallet
-              </h6>
-              <h6 className="font-weight-light name-text mb-2">
-                $
-                {this.props.loading ? (
-                  <Spinner
-                    className="ml-2"
-                    variant="success"
-                    animation="border"
-                    size="sm"
-                  />
-                ) : (
-                  this.props.formatter().format(this.props.wallet)
-                )}
-              </h6>
-            </div>
-          </div>
+      <div className="container-fluid d-block container user-container">
+        <h4 className="d-inline-block ml-3 mr-3 heading-text">
+          User Information
+        </h4>
+        <Button
+          onClick={this.props.handleShow}
+          className="buy-sell-button"
+          variant="success"
+        >
+          <span className="font-weight-light">Buy/Sell Stocks</span>
+        </Button>{" "}
+        <hr style={{ borderTop: "1px solid #1aac3c", width: "100%" }} />
+        <div className="col-12">
+          <h6 className="font-weight-normal d-inline-block mb-1 titles-text">
+            User{" "}
+          </h6>
+          <h6 className="font-weight-light d-block mb-2 name-text">
+            {this.props.loading ? (
+              <Spinner
+                className="ml-2"
+                variant="success"
+                animation="border"
+                size="sm"
+              />
+            ) : (
+              this.props.username
+            )}
+          </h6>
+          <h6
+            id="holdings"
+            className="font-weight-normal d-inline-block mb-1 titles-text"
+          >
+            Holdings:
+          </h6>
+          <h6
+            id="holding-text"
+            className="font-weight-light d-block mb-2 name-text"
+          >
+            $
+            {this.props.loading ? (
+              <Spinner
+                className="ml-2"
+                variant="success"
+                animation="border"
+                size="sm"
+              />
+            ) : (
+              this.props.formatter().format(this.props.holdings)
+            )}
+          </h6>
         </div>
+        <div className="col-5">
+          <h6
+            id="wallet"
+            className="font-weight-normal d-inline-block mb-1 titles-text"
+          >
+            Wallet
+          </h6>
+          <h6 className="font-weight-light name-text mb-2">
+            $
+            {this.props.loading ? (
+              <Spinner
+                className="ml-2"
+                variant="success"
+                animation="border"
+                size="sm"
+              />
+            ) : (
+              this.props.formatter().format(this.props.wallet)
+            )}
+          </h6>
+        </div>
+      </div>
     );
   }
 }
