8b1c6c120de23343a34d7918ac9f5f1b4f9c298d
ram071985 <reidmuchow@gmail.com>
2021-01-13 11:21:55 -0600

    added protected route component tsx.

--- API/client/src/Components/ProtectedRoute.tsx
+++ API/client/src/Components/ProtectedRoute.tsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 
-class ProtectedRoute extends Component {
+interface IProtectedRoute {
+    component: any
+}
+
+class ProtectedRoute extends Component<IProtectedRoute, any> {
   render() {
       const Component = this.props.component;
       const isAuthenticated = localStorage.getItem("session_id");
