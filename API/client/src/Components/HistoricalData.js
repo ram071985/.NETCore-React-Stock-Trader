d6e80be1e4d23f03945a722bfe97c794905b6b39
ram071985 <reidmuchow@gmail.com>
2021-02-05 10:09:02 -0600

    added canvas, initialization, and componenetDidMount for charts.

--- API/client/src/Components/HistoricalData.js
+++ API/client/src/Components/HistoricalData.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
-import { Row, Alert } from "react-bootstrap";
+import Chart from "chart.js";
 
 class HistoricalData extends Component {
   constructor() {
     super();
     this.state = {};
   }
+  chartRef = React.createRef();
+
+  componentDidMount() {
+    const myChartRef = this.chartRef.current.getContext("2d");
+    new Chart(myChartRef, {
+      type: "line",
+      data: {
+        //Bring in data
+        labels: ["Jan", "Feb", "March"],
+        datasets: [
+          {
+            label: "Sales",
+            data: [86, 67, 91],
+          },
+        ],
+      },
+      options: {
+        //Customize chart options
+      },
+    });
+  }
   render() {
     return (
       <div className="container-fluid d-block container user-container">
-          <h1>Stuff should go in here bitches.</h1>
+        <canvas id="myChart" ref={this.chartRef} />
       </div>
     );
   }
